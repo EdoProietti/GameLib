195701c00cb22076ed4a73a605633102123e0566
EdoProietti <edoardoproietti03@gmail.com>
2026-01-27 21:10:22 +0100

    Fixed some code smell.

---
+++ src/main/java/Model/Game/GameDBDAO.java
@@ -0,0 +1,24 @@
+package Model.Game;
+
+import Model.User.Publisher;
+
+import java.util.List;
+
+public class GameDBDAO {
+
+    public Game getGame(String title, String publisher){
+        return null;
+    }
+
+    public List<Game> getGames(String title){
+        return null;
+    }
+
+    void addGame(Game game){
+        return;
+    }
+
+    List<Game> getPublisherGames(Publisher publisher){
+
+    }
+}
